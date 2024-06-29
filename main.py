15e56857e32038fac4d9dc7987c42c2e0cb94e05
nuram <nuraam2003@gmail.com>
2024-06-29 19:27:57 -0400

    refactor

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, str]
 
 @app.get("/")
 async def root():
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q" : q}
+
+@app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.put('/items/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item": item}
